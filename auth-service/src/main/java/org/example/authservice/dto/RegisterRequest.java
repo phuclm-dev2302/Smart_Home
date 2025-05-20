cfbea7c05f9304188cfd49f60a9defd0a13ab319
Phuc <lephuc04b3@gmail.com>
2025-05-20 21:21:10 +0700

    feat: add RegisterRequest

---
+++ auth-service/src/main/java/org/example/authservice/dto/RegisterRequest.java
@@ -0,0 +1,10 @@
+package org.example.authservice.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
+}
