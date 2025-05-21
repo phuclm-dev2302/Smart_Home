3d2d7f708156f3b13985e854c7c1d3f71bd7144b
Phuc <lephuc04b3@gmail.com>
2025-05-21 17:42:16 +0700

    refactor: delete target

--- auth-service/src/main/java/org/example/authservice/config/WebClientConfig.java
+++ auth-service/src/main/java/org/example/authservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package org.example.authservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public WebClient webClient(WebClient.Builder builder) {
         return builder.build();
     }
