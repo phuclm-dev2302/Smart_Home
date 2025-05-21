8b566e41b8c58157369f74abf45770edd5a91110
Phuc <lephuc04b3@gmail.com>
2025-05-21 17:34:58 +0700

    feat: init post-service

---
+++ post-service/src/main/java/org/example/postservice/service/Impl/PostServiceImpl.java
@@ -0,0 +1,8 @@
+package org.example.postservice.service.Impl;
+
+import org.example.postservice.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl  {
+}
---
+++ post-service/src/main/java/org/example/postservice/service/PostService.java
@@ -0,0 +1,4 @@
+package org.example.postservice.service;
+
+public interface PostService {
+}
