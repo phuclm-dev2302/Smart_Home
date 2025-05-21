f78589faf4ad83f8ac3e37f207fda5fdbffd29b8
Phuc <lephuc04b3@gmail.com>
2025-05-21 17:33:23 +0700

    feat: add webclientconfig

---
+++ post-service/src/main/java/org/example/postservice/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package org.example.postservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class WebClientConfig {
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
+}
