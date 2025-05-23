3dd27c2b0571a952c696f0570f5d83f315731472
Phuc <lephuc04b3@gmail.com>
2025-05-23 14:10:01 +0700

    feat: add method getUserIdFromToken and createPost

--- post-service/src/main/java/org/example/postservice/service/PostService.java
+++ post-service/src/main/java/org/example/postservice/service/PostService.java
@@ -1,4 +1,12 @@
 package org.example.postservice.service;
 
+import org.example.postservice.dto.request.PostDetailRequest;
+import org.example.postservice.dto.request.PostRequest;
+import org.example.postservice.dto.response.PostResponse;
+import org.example.postservice.model.Post;
+import reactor.core.publisher.Mono;
+
 public interface PostService {
+    Mono<String> getUserIdFromToken();
+    Mono<PostResponse> createPost(PostRequest request);
 }
