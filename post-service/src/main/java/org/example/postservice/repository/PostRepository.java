dbc16fb288ecaa7b3e0dbd272b369378f714d4a8
Phuc <lephuc04b3@gmail.com>
2025-05-23 14:08:49 +0700

    feat: add PostRepository

--- post-service/src/main/java/org/example/postservice/repository/PostRepository.java
+++ post-service/src/main/java/org/example/postservice/repository/PostRepository.java
@@ -1,4 +1,11 @@
 package org.example.postservice.repository;
 
-public interface PostRepository {
+import org.example.postservice.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
 }
