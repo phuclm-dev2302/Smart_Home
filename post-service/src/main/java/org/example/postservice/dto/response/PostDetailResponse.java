802961c3ac97cd47558a07db809f72ef0cc946dc
Phuc <lephuc04b3@gmail.com>
2025-05-23 14:09:05 +0700

    feat: add PostDetailResponse

---
+++ post-service/src/main/java/org/example/postservice/dto/response/PostDetailResponse.java
@@ -0,0 +1,37 @@
+package org.example.postservice.dto.response;
+
+import lombok.Data;
+import org.example.postservice.model.PostDetail;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class PostDetailResponse {
+    private BigDecimal price;
+    private BigDecimal area;
+    private String length;
+    private String horizontal;
+    private int bedRoom;
+    private int bathRoom;
+    private int floor;
+    private boolean legalPapers;
+
+    private List<AmenityResponse> amenities;
+
+    public static PostDetailResponse toDto(PostDetail postDetail, List<AmenityResponse> amenities) {
+        PostDetailResponse response = new PostDetailResponse();
+        response.setPrice(postDetail.getPrice());
+        response.setArea(postDetail.getArea());
+        response.setLength(postDetail.getLength());
+        response.setHorizontal(postDetail.getHorizontal());
+        response.setBedRoom(postDetail.getBedRoom());
+        response.setBathRoom(postDetail.getBathRoom());
+        response.setFloor(postDetail.getFloor());
+        response.setLegalPapers(postDetail.isLegalPapers());
+        response.setAmenities(amenities);
+        return response;
+    }
+
+
+}
