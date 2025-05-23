87f13d6a16c8f47972c729f7e42c1cb972df081e
Phuc <lephuc04b3@gmail.com>
2025-05-23 14:07:54 +0700

    feat: add CreateAmenityRequest DTO

---
+++ post-service/src/main/java/org/example/postservice/dto/request/CreateAmenityRequest.java
@@ -0,0 +1,9 @@
+package org.example.postservice.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CreateAmenityRequest {
+    private String name;   // Wifi, Dieu hoa
+    private String description; // Wifi 24/7
+}
