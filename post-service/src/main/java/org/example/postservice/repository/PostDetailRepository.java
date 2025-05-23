b57d00d21f8ddf7a0e1f89c6e60f5f064a9ef98f
Phuc <lephuc04b3@gmail.com>
2025-05-23 14:08:30 +0700

    feat: add CreateAmenityRequest DTO

---
+++ post-service/src/main/java/org/example/postservice/repository/PostDetailRepository.java
@@ -0,0 +1,10 @@
+package org.example.postservice.repository;
+
+import org.example.postservice.model.PostDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface PostDetailRepository extends JpaRepository<PostDetail, UUID> {
+}
