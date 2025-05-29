7f0824baf132e5fce0ff20ca8ab054d03a1a57d5
Phuc <lephuc04b3@gmail.com>
2025-05-29 16:41:17 +0700

    feat: add endpoint get-post-by-id

--- post-service/src/main/java/org/example/postservice/controller/PostController.java
+++ post-service/src/main/java/org/example/postservice/controller/PostController.java
@@ -5,8 +5,11 @@ import org.example.postservice.dto.response.PostResponse;
 import org.example.postservice.model.Post;
 import org.example.postservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -20,10 +23,13 @@ public class PostController {
         return postService.getUserIdFromToken()
                 .map(userId -> userId);
     }
-
     @PostMapping("")
     public Mono<PostResponse> createPost(@RequestBody PostRequest request) {
         return postService.createPost(request);
+    }
+    @GetMapping("/{id}")
+    public Mono<PostResponse> getPostById(@PathVariable UUID id){
+        return postService.getPostById(id);
     }
 
 }
