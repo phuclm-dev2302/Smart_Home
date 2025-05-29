4ed4ba7ccefdea72e53d21763497ee102dcfc71f
Phuc <lephuc04b3@gmail.com>
2025-05-29 16:40:09 +0700

    feat: add JPA findById(UUID id) in PostRepository

--- post-service/src/main/java/org/example/postservice/repository/PostRepository.java
+++ post-service/src/main/java/org/example/postservice/repository/PostRepository.java
@@ -4,8 +4,10 @@ import org.example.postservice.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
+    Optional<Post> findById(UUID id);
 }
