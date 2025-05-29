efd3776952bd81cde5283c66853437443b8987c8
Phuc <lephuc04b3@gmail.com>
2025-05-29 18:04:50 +0700

    feat: add endpoint getAllPost [Mono<List<PostResponse>>]

--- post-service/src/main/java/org/example/postservice/controller/PostController.java
+++ post-service/src/main/java/org/example/postservice/controller/PostController.java
@@ -2,13 +2,12 @@ package org.example.postservice.controller;
 
 import org.example.postservice.dto.request.PostRequest;
 import org.example.postservice.dto.response.PostResponse;
-import org.example.postservice.model.Post;
 import org.example.postservice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -23,13 +22,23 @@ public class PostController {
         return postService.getUserIdFromToken()
                 .map(userId -> userId);
     }
+
     @PostMapping("")
     public Mono<PostResponse> createPost(@RequestBody PostRequest request) {
         return postService.createPost(request);
     }
+
     @GetMapping("/{id}")
-    public Mono<PostResponse> getPostById(@PathVariable UUID id){
+    public Mono<PostResponse> getPostById(@PathVariable UUID id) {
         return postService.getPostById(id);
+    }
+
+    @GetMapping("")
+    public Mono<List<PostResponse>> getAllPost(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return postService.getAllPost(page, size);
     }
 
 }
