23e7eaa37733ca1dc6da80b5fa78001e41cbc40f
Phuc <lephuc04b3@gmail.com>
2025-05-29 16:36:58 +0700

    feat: add query findAllByPostDetailId in Amenities

--- amenity-service/src/main/java/org/example/amenityservice/repository/AmenityRepository.java
+++ amenity-service/src/main/java/org/example/amenityservice/repository/AmenityRepository.java
@@ -2,10 +2,17 @@ package org.example.amenityservice.repository;
 
 import org.example.amenityservice.model.Amenity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AmenityRepository extends JpaRepository<Amenity, UUID> {
+    @Query("SELECT a FROM Amenity a " +
+            "WHERE a.postDetailId = :postDetailId")
+    List<Amenity> findAllByPostDetailId(@Param("postDetailId") UUID postDetailId);
+
 }
