86239c309f99ca5dcc4d616114d67a5bb3e73b21
Phuc <lephuc04b3@gmail.com>
2025-06-01 14:50:50 +0700

    refactor: add field data to Event

--- common-event/src/main/java/org/example/commonevent/common/event/CreatePostDocumentEvent.java
+++ common-event/src/main/java/org/example/commonevent/common/event/CreatePostDocumentEvent.java
@@ -5,12 +5,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CreatePostDocumentEvent {
     private UUID id;
     private String token;
+    private String title;
+    private String description;
+    private String city;
+    private String district;
+    private String address;
+    private String ward;
+    private String price;
+    private String area;
+    private String bedRoom;
+    private String bathRoom;
+    private String floor;
+    private String legalPapers;
+    private List<String> amenities;
+    private String postType;
+    private String status;
+    private String createAt;
+
 }
