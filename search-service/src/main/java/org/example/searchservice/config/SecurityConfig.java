ec7e5365acd3e00af0f591ceb97e1d59571a61a1
Phuc <lephuc04b3@gmail.com>
2025-06-01 14:45:24 +0700

    feat: add securityConfig using spring webflux

---
+++ search-service/src/main/java/org/example/searchservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.example.searchservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/v1/search/**").authenticated() // ac thuc cho api post
+                        .anyExchange().permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+}
