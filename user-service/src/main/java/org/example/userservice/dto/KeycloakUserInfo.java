6b11ddb8c6a808186838db39cd06abda449d8d73
NguyenLam1708 <lam170804@gmail.com>
2025-06-14 19:47:05 +0700

    refactor: delete attribute address

--- user-service/src/main/java/org/example/userservice/dto/KeycloakUserInfo.java
+++ user-service/src/main/java/org/example/userservice/dto/KeycloakUserInfo.java
@@ -1,16 +1,12 @@
 package org.example.userservice.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class KeycloakUserInfo {
     private String sub;
-    @JsonProperty("preferred_username")
-    private String preferredUsername;
+    private String preferred_username;
     private String name;
     private String email;
-    @JsonProperty("phone_number")
     private String phoneNumber;
-    private String address;
 }
