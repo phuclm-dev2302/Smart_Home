0c70835463982475e206a492da7f08df0cbf82e3
NguyenLam1708 <lam170804@gmail.com>
2025-06-14 19:46:43 +0700

    refactor: fix bug cors

--- api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -2,15 +2,10 @@ package org.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-
-import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,33 +15,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity
+        return serverHttpSecurity
                 .csrf(csrfSpec -> csrfSpec.disable())
-                .cors(withDefaults())
+                .cors(withDefaults())  // Chỉ bật để Security biết có CORS, header do Gateway trả
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/eureka/**",
-                                "/api/v1/auth/**"
-                        )
-                        .permitAll()
-                        .anyExchange()
-                        .authenticated()
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Cho phép preflight
+                        .pathMatchers("/eureka/**", "/api/v1/auth/**").permitAll()
+                        .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
-
-        return serverHttpSecurity.build();
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                .build();
     }
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration cors = new CorsConfiguration();
-        cors.setAllowedOriginPatterns(List.of("*"));   // origin cụ thể
-        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        cors.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        cors.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", cors);
-
-        return new CorsWebFilter(source);
-    }
 }
