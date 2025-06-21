e9be981ca6e06061944db10eab949385eb4f70a3
NguyenLam1708 <lam170804@gmail.com>
2025-06-21 11:23:02 +0700

    refactor: delete imageId

--- post-service/src/main/java/org/example/postservice/dto/request/PostRequest.java
+++ post-service/src/main/java/org/example/postservice/dto/request/PostRequest.java
@@ -3,12 +3,11 @@ package org.example.postservice.dto.request;
 import lombok.Data;
 import org.example.postservice.enums.PostTypeEnums;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
 public class PostRequest {
-
-    private UUID imageId;
 
     private String title;
     private String description;
