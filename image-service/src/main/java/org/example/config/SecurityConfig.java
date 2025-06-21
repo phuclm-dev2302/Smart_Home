4d7e0a53a2116df88fed4c8c0f7c217b92a81a48
NguyenLam1708 <lam170804@gmail.com>
2025-06-21 11:21:17 +0700

    feat : add securityconfig

---
+++ image-service/src/main/java/org/example/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.example.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+// AuthSecurityConfig.java
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/api/v1/images/**","/uploads/**").permitAll()
+                        .anyExchange().denyAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+        return http.build();
+    }
+}
