eb4fa9ce83a30f2246ab561c49bb9581d0c563fe
NguyenLam1708 <lam170804@gmail.com>
2025-06-24 23:51:28 +0700

    refactor: config no authentication for getPostById and getAllPost

--- post-service/src/main/java/org/example/postservice/config/SecurityConfig.java
+++ post-service/src/main/java/org/example/postservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.postservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -17,9 +18,13 @@ public class SecurityConfig {
         http
                 .csrf(csrfSpec -> csrfSpec.disable())
                 .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/api/v1/posts/**").authenticated() // ac thuc cho api post
+                        .pathMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/api/v1/posts/**").authenticated()
+                        .pathMatchers(HttpMethod.PUT, "/api/v1/posts/**").authenticated()
+                        .pathMatchers(HttpMethod.DELETE, "/api/v1/posts/**").authenticated()
                         .anyExchange().permitAll())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
         return http.build();
     }
+
 }
