8c2ccf16d1d951595912570f35402674a6030bad
NguyenLam1708 <lam170804@gmail.com>
2025-06-29 12:18:30 +0700

    refactor: method get for user-service no authentication

--- api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Cho phép preflight
                         .pathMatchers("/favicon.ico").permitAll()
                         .pathMatchers("/eureka/**", "/api/v1/auth/**","/uploads/image/**").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/api/v1/posts/**","/api/v1/images/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/v1/posts/**","/api/v1/images/**","/api/v1/users/**").permitAll()
                         .pathMatchers(HttpMethod.POST, "/api/v1/ai/**").permitAll()
                         .anyExchange().authenticated()
                 )
